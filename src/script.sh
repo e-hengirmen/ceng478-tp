5fc14ca43c2ee98c72371f605b1f007c13a8316e
e-hengirmen <ehengirmen@hotmail.com>
2021-06-26 00:53:56 +0300

    report almost done add how to use the code

--- src/script.sh
+++ src/script.sh
@@ -1,11 +1,11 @@
-mpicc solution1.c -o parallel -lm
-gcc serial1.c -o sequential -lm
+mpicc solution.c -o parallel -lm
+gcc serial.c -o sequential -lm
 mpirun -np 4 ./parallel
 ./sequential <input
 echo "diff is executed for outputs of parallel and sequential algorithms below"
 echo "if you see any differences it means that one of the algoritms are wrong"
 echo "differences are:"
-echo ""
 diff output output2
+echo ""
 echo "Initial matrices(A and B) of matrix multiplication has been stored in input file"
 echo "Output matrices from parallel and sequential codes are stored in output and output2 files"
